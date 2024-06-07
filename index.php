ceacb62a4bb3bda53af5ccd272c7c555860f3825
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-07 10:47:08 +0500

    update html, css

--- index.php
+++ index.php
@@ -10,14 +10,18 @@ require_once 'templates/functions.php';
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-    <div class="super">
-        <h1>My_first_work_in_GitHub!!!</h1>
+    <div class="wrapper">
+        <div class="container">
+
+        </div>
     </div>
 </body>
+<script type="text/javascript" src="js/script.js"></script>
 
 </html>
