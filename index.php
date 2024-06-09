6045b8f590bf9be021507e47065f5da45b38cd28
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-09 11:00:56 +0500

    edit code

--- index.php
+++ index.php
@@ -4,12 +4,33 @@ require_once 'templates/connect.php';
 require_once 'templates/functions.php';
 // session_start();
 
-$query = "SELECT * FROM `users`";
-$res = $pdo->query($query);
-$data = $res->fetchAll();
+// $query = "SELECT * FROM `users`";
+// $res = $pdo->query($query);
+// $data = $res->fetchAll();
 
-/* $write = "INSERT INTO `users` SET `login` = 'gleb', `pass` = 'Gleb', `name` = 'Gleb', `age` = 25";
-$pdo->exec($write); */
+// $write = "INSERT INTO `users` SET `login` = 'gleb', `pass` = 'Gleb', `name` = 'Gleb', `age` = 25";
+// $pdo->exec($write);
+
+$login = 'vladimir';
+$pass = password_hash('Vladimir', PASSWORD_DEFAULT);
+$name = 'Vladimir';
+$age = 38;
+
+$param = [
+    'l' => $login,
+    'p' => $pass,
+    'n' => $name,
+    'a' => $age
+];
+
+// $query = "INSERT INTO `users`(`login`, `pass`, `name`, `age`) VALUES(?, ?, ?, ?)";
+// $result = $pdo->prepare($query);
+// $result->execute([$login, $pass, $name, $age]);
+
+// $query = "INSERT INTO `users`(`login`, `pass`, `name`, `age`) VALUES(:l, :p, :n, :a)";
+// $result = $pdo->prepare($query);
+// $result->execute($param);
+
 ?>
 
 <!DOCTYPE html>
@@ -31,9 +52,9 @@ $pdo->exec($write); */
             <hr> -->
         </div>
         <div class="container">
-            <?php foreach ($data as $key => $value) : ?>
-                <!-- <p>Name: <?= $value['login'] . ',' . $value['pass'] . ',' . $value['name'] . ',' . $value['age'] . ',' . $value['date_reg'] ?></p> -->
-            <?php endforeach; ?>
+            <!-- <?php foreach ($data as $key => $value) : ?>
+                <p>Name: <?= $value['login'] . ',' . $value['pass'] . ',' . $value['name'] . ',' . $value['age'] . ',' . $value['date_reg'] ?></p>
+            <?php endforeach; ?> -->
         </div>
     </div>
 </body>
