567aa972103e7cf339aa77d3adc23d9cfa9e1a5d
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-10 20:11:08 +0500

    edit files: index.php and style.css

--- index.php
+++ index.php
@@ -45,35 +45,65 @@ $param = [
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="container wrapper">
-            <header class="container-fluid header">
-                <div class="column">
-                    <div class="col-4 header-logotype">
-                        <h1 class="">MyBlog</h1>
+    <div class="container flex-column wrapper">
+        <header class="container-fluid header p-2">
+            <!-- <div class="row"> -->
+            <nav class="navbar navbar-expand-md">
+                <div class="container-fluid">
+                    <a class="navbar-brand header-logo" href="/">MyBlog</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="/">Главная</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">О нас</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Услуги</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Контакты</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-user"></i> Вход</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Админ панель</a></li>
+                                    <li><a class="dropdown-item" href="#">Выход</a></li>
+                                    <!-- <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Что-то еще здесь</a></li> -->
+                                </ul>
+                            </li>
+                            <!--  <li class="nav-item">
+                                    <a class="nav-link link-primary" href="#"><i class="fa-solid fa-user"> Вход</a>
+                                </li> -->
+                        </ul>
                     </div>
-                    <div class="col-8 header-menu">
-
-                    </div>
                 </div>
-            </header>
+            </nav>
+            <!-- </div> -->
+        </header>
 
-            <main class="container-fluid main">
-                <div class="row">
-                    <p class=""></p>
-                </div>
-            </main>
-            <footer class="container-fluid footer">
-                <div class="row">
-                    <p class="text-center">copyrighted@2020-<?= date('Y') ?></p>
-                </div>
-            </footer>
-        </div>
-
+        <main class="container-fluid main p-2">
+            <!-- <div class="row"> -->
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis magnam cumque saepe aperiam quam, voluptate, aliquid, quia temporibus amet provident animi aliquam facilis iure explicabo iste cupiditate ipsam. Illum.</p>
+            <!-- </div> -->
+        </main>
+        <footer class="container-fluid d-flex align-items-center footer p-2">
+            <!-- <div class="row d-block"> -->
+            <h4 class="col-12 text-center">copyrighted@2020-<?= date('Y') ?></h4>
+            <!-- </div> -->
+        </footer>
     </div>
 
 </body>
-<script type="text/javascript" src="js/script.js"></script>
 <script type="text/javascript" src="bootstrap_5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/5cc4ae0ce8.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/script.js"></script>
 
 </html>
