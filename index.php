17a868b85dd9c3ce9f79fb64a28d53590f6fe0b3
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-10 14:39:55 +0500

    edit files .gitignore and index.php

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ $param = [
     <meta charset="UTF-8">
     <title>Document</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap_5.3.3/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
