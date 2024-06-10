152e8a29d36afe21aec2b7e4ee04ef86f9468276
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-10 16:03:41 +0500

    edit files: index.php, style.css. Add files logotype

--- index.php
+++ index.php
@@ -38,26 +38,40 @@ $param = [
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Engin in PHP</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap_5.3.3/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="container">
-            <!-- <p>Имя: <?= $data['name'] ?></p>
-            <p>Возраст: <?= $data['age'] ?></p>
-            <p>Дата регистрации: <?= $data['date_reg'] ?></p>
-            <hr> -->
+    <div class="container-fluid">
+        <div class="container wrapper">
+            <header class="container-fluid header">
+                <div class="column">
+                    <div class="col-4 header-logotype">
+                        <h1 class="">MyBlog</h1>
+                    </div>
+                    <div class="col-8 header-menu">
+
+                    </div>
+                </div>
+            </header>
+
+            <main class="container-fluid main">
+                <div class="row">
+                    <p class=""></p>
+                </div>
+            </main>
+            <footer class="container-fluid footer">
+                <div class="row">
+                    <p class="text-center">copyrighted@2020-<?= date('Y') ?></p>
+                </div>
+            </footer>
         </div>
-        <div class="container">
-            <!-- <?php foreach ($data as $key => $value) : ?>
-                <p>Name: <?= $value['login'] . ',' . $value['pass'] . ',' . $value['name'] . ',' . $value['age'] . ',' . $value['date_reg'] ?></p>
-            <?php endforeach; ?> -->
-        </div>
+
     </div>
+
 </body>
 <script type="text/javascript" src="js/script.js"></script>
 <script type="text/javascript" src="bootstrap_5.3.3/js/bootstrap.bundle.min.js"></script>
