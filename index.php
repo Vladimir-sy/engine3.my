2bff837c74e157822d081474f49891efd43c53f2
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-12 15:11:48 +0500

    edit files style.css and index.php

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ $param = [
 </head>
 
 <body>
-    <div class="container flex-column wrapper">
-        <header class="container-fluid header p-2">
+    <div class="container wrapper">
+        <header class="container-fluid header p-2 mb-2">
             <!-- <div class="row"> -->
             <nav class="navbar navbar-expand-md">
                 <div class="container-fluid">
@@ -89,14 +89,70 @@ $param = [
             <!-- </div> -->
         </header>
 
-        <main class="container-fluid main p-2">
-            <!-- <div class="row"> -->
-            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis magnam cumque saepe aperiam quam, voluptate, aliquid, quia temporibus amet provident animi aliquam facilis iure explicabo iste cupiditate ipsam. Illum.</p>
-            <!-- </div> -->
-        </main>
-        <footer class="container-fluid d-flex align-items-center footer p-2">
+        <section class="container-md col-lg-9 mb-2">
+            <div class="row">
+                <h2 class="text-center">Топ публикации</h2>
+            </div>
+            <div id="carouselExampleCaptions" class="carousel slide">
+                <!-- <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div> -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/img6_1.jpg" class="d-block w-100" alt="Image">
+                        <div class="carousel-caption-edit d-none d-md-block">
+                            <h5><a class="nav-link" href="#">Метка первого слайда</a></h5>
+                            <!-- <p>Некоторый репрезентативный заполнитель для первого слайда.</p> -->
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/img7_1.jpg" class="d-block w-100" alt="Image">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Метка второго слайда</h5>
+                            <!-- <p>Некоторый репрезентативный заполнитель для второго слайда.</p> -->
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/img6.jpg" class="d-block w-100" alt="Image">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Метка третьего слайда</h5>
+                            <!-- <p>Некоторый репрезентативный заполнитель для третьего слайда.</p> -->
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Предыдущий</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Следующий</span>
+                </button>
+            </div>
+        </section>
+
+        <section class="content container-fluid d-flex flex-wrap p-2 mb-2 gx-5">
+            <main class="main col-12 col-md-9 p-2">
+                <!-- <div class="row"> -->
+                <p class="align-">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis magnam cumque saepe aperiam quam, voluptate, aliquid, quia temporibus amet provident animi aliquam facilis iure explicabo iste cupiditate ipsam. Illum.</p>
+                <!-- </div> -->
+            </main>
+            <aside class="aside col-12 col-md-3 p-2 text-wrap">
+                <h2>Slider</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad, culpa possimus perferendis reiciendis alias, reprehenderit praesentium nisi pariatur sed quo! Neque nulla cumque nobis sint esse, minus omnis molestiae.</p>
+            </aside>
+        </section>
+
+        <footer class=" container-fluid d-flex align-items-center footer p-2 flex-wrap">
             <!-- <div class="row d-block"> -->
-            <h4 class="col-12 text-center">copyrighted@2020-<?= date('Y') ?></h4>
+            <div class="col-12 col-md-9 text-start">
+                <h4 class="text-center">copyrighted&#174; 2020-<?= date('Y') ?></h4>
+            </div>
+            <div class="col-12 col-md-3 text-center">
+                <a class="nav-link" href="/">MyBlog</a>
+            </div>
             <!-- </div> -->
         </footer>
     </div>
