933e488aca0f48ba34a75210fecccfd07424bed1
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-12 17:35:41 +0500

    edit files

--- index.php
+++ index.php
@@ -109,14 +109,14 @@ $param = [
                     </div>
                     <div class="carousel-item">
                         <img src="images/img7_1.jpg" class="d-block w-100" alt="Image">
-                        <div class="carousel-caption d-none d-md-block">
+                        <div class="carousel-caption-edit d-none d-md-block">
                             <h5>Метка второго слайда</h5>
                             <!-- <p>Некоторый репрезентативный заполнитель для второго слайда.</p> -->
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="images/img6.jpg" class="d-block w-100" alt="Image">
-                        <div class="carousel-caption d-none d-md-block">
+                        <img src="images/img16_1.jpg" class="d-block w-100" alt="Image">
+                        <div class="carousel-caption-edit d-none d-md-block">
                             <h5>Метка третьего слайда</h5>
                             <!-- <p>Некоторый репрезентативный заполнитель для третьего слайда.</p> -->
                         </div>
@@ -136,11 +136,12 @@ $param = [
         <section class="content container-fluid d-flex flex-wrap p-2 mb-2 gx-5">
             <main class="main col-12 col-md-9 p-2">
                 <!-- <div class="row"> -->
+                <h3 class="text-center">Content</h3>
                 <p class="align-">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis magnam cumque saepe aperiam quam, voluptate, aliquid, quia temporibus amet provident animi aliquam facilis iure explicabo iste cupiditate ipsam. Illum.</p>
                 <!-- </div> -->
             </main>
-            <aside class="aside col-12 col-md-3 p-2 text-wrap">
-                <h2>Slider</h2>
+            <aside class="aside col-12 col-md-3 p-2 text-wrap d-none d-md-block">
+                <h3 class="text-center">Slider</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad, culpa possimus perferendis reiciendis alias, reprehenderit praesentium nisi pariatur sed quo! Neque nulla cumque nobis sint esse, minus omnis molestiae.</p>
             </aside>
         </section>
