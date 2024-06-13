d26d50ccb0b5dbba3f0265914e2bab630ff2ebe9
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-13 16:58:03 +0500

    edit files

--- index.php
+++ index.php
@@ -134,55 +134,111 @@ $param = [
         </section>
 
         <section class="content container-fluid d-flex flex-wrap p-2 mb-2 gx-5">
-            <main class="main col-12 col-md-9 p-2">
-                <h3 class="text-center">Последние публикации</h3>
-                <div class="post row mb-2">
+            <div class="content-title col-12 ">
+                <h2 class="main-title col-12 text-center text-md-start">Последние публикации</h2>
+            </div>
+            <main class="main col-12 col-md-8 p-2 ">
+                <div class="post d-block d-md-flex mb-3 p-2">
                     <div class="post-image col-12 col-md-4 text-center mb-2 mb-md-0">
                         <img src="images/img17_1.jpg" alt="Image" class="img-thumbnail">
                     </div>
-                    <div class="post-text col-12 col-md-8 ">
-                        <h2>
-                            <a class="nav-link" href="#">Статья на тему динамического сайта...</a>
-                        </h2>
+                    <div class="post-text col-12 col-md-8 p-2">
+                        <h3>
+                            <a class="nav-link link-underline" href="#">Статья на тему динамического сайта...</a>
+                        </h3>
                         <i class="far fa-user"> Имя Автора </i>
                         <i class="far fa-calendar"> Mar 11, 2024</i>
-                        <p class="preview-text">
+                        <p class="preview-text d-block">
                             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt minima repellat, voluptates placeat molestias architecto excepturi voluptatem recusandae sunt qui impedit quibusdam possimus tenetur ratione! Ut molestias voluptatum nostrum dolorum?
                         </p>
                     </div>
-                    <hr>
                 </div>
-                <div class="post row mb-2">
+                <div class="post d-block d-md-flex mb-3 p-2">
                     <div class="post-image col-12 col-md-4 text-center mb-2 mb-md-0">
                         <img src="images/img18_1.jpg" alt="Image" class="img-thumbnail">
                     </div>
-                    <div class="post-text col-12 col-md-8 ">
-                        <h2>
+                    <div class="post-text col-12 col-md-8 p-2">
+                        <h3>
                             <a class="nav-link" href="#">Статья на тему динамического сайта...</a>
-                        </h2>
+                        </h3>
                         <i class="far fa-user"> Имя Автора </i>
                         <i class="far fa-calendar"> Mar 11, 2024</i>
                         <p class="preview-text">
                             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt minima repellat, voluptates placeat molestias architecto excepturi voluptatem recusandae sunt qui impedit quibusdam possimus tenetur ratione! Ut molestias voluptatum nostrum dolorum?
                         </p>
                     </div>
-                    <hr>
                 </div>
             </main>
-            <aside class="aside col-12 col-md-3 p-2 text-wrap d-none d-md-block">
-
+            <aside class="aside col-12 col-md-4 p-2 text-wrap d-none d-md-block">
+                <div class="col-12 section search mb-3 p-2">
+                    <h3 class="">Поиск</h3>
+                    <form class="form-search d-block" role="search">
+                        <input class="form-control mb-2 search-input" type="search" name="search-term" placeholder="Поиск" aria-label="Поиск">
+                        <button class="btn btn-outline-success mb-2 search-btn" type="submit">Поиск</button>
+                    </form>
+                </div>
+                <div class="col-12 section topics mb-3 p-2 text-wrap">
+                    <h3 class="">Категории</h3>
+                    <ul class="aside-list">
+                        <li><a class="nav-link p-1" href="#">Poems</a></li>
+                        <li><a class="nav-link p-1" href="#">Quotes</a></li>
+                        <li><a class="nav-link p-1" href="#">Fiction</a></li>
+                        <li><a class="nav-link p-1" href="#">Biography</a></li>
+                        <li><a class="nav-link p-1" href="#">Motivation</a></li>
+                        <li><a class="nav-link p-1" href="#">Inspiration</a></li>
+                        <li><a class="nav-link p-1" href="#">Life Lessons</a></li>
+                    </ul>
+                </div>
             </aside>
         </section>
 
-        <footer class=" container-fluid d-flex align-items-center footer p-2 flex-wrap">
-            <!-- <div class="row d-block"> -->
-            <div class="col-12 col-md-9 text-start">
-                <h4 class="text-center">copyrighted&#174; 2020-<?= date('Y') ?></h4>
+        <footer class="footer container-fluid d-flex align-items-center footer p-2 flex-wrap">
+            <div class="footer-about col-12 col-md-4 mb-2 mb-md-0 p-2 align-self-start">
+                <h3 class="logo-text text-center text-md-start">Мой блог</h3>
+                <p>Мой блог - это обучение создания сайта на PHP, MySQL</p>
+                <div class="contact p-1">
+                    <h5>Контакты:</h5>
+                    <p><i class="fas fa-phone"></i>&nbsp; <span> 8-982-901-82-45</span></p>
+                    <p><i class="fas fa-envelope"></i>&nbsp; <span> info@engine3.my</span></p>
+
+                </div>
+                <div class="socials p-1">
+                    <h5>Соц. сети:</h5>
+                    <a class="p-2" href="#"><i class="fab fa-youtube"></i></a>
+                    <a class="p-2" href="#"><i class="fab fa-instagram"></i></a>
+                    <a class="p-2" href="#"><i class="fab fa-facebook"></i></a>
+                    <a class="p-2" href="#"><i class="fab fa-twitter"></i></a>
+                </div>
             </div>
-            <div class="col-12 col-md-3 text-center">
-                <a class="nav-link" href="/">MyBlog</a>
+            <div class="footer-links col-12 col-md-4 mb-2 mb-md-0 p-2 align-self-start">
+                <h3 class="text-center text-md-start">Популярные ссылки</h3>
+                <div class="">
+                    <!-- <ul> -->
+                    <a class="nav-link p-1" href="#">События</a></li>
+                    <a class="nav-link p-1" href="#">Команда</a></li>
+                    <a class="nav-link p-1" href="#">Упражнения</a></li>
+                    <a class="nav-link p-1" href="#">Галерея</a></li>
+                    <a class="nav-link p-1" href="#">Другое</a></li>
+                    <!-- </ul> -->
+                </div>
             </div>
-            <!-- </div> -->
+            <div class="footer-feedback col-12 col-md-4 mb-2 mb-md-0 p-2 align-self-start">
+                <h3 class="text-center text-md-start">Обратная связь</h3>
+                <form>
+                    <div class="mb-3">
+                        <!-- <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label> -->
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите эл. почту (info@mail.ru)">
+                    </div>
+                    <div class="mb-3">
+                        <!-- <label for="exampleInputTextArea1" class="form-label">Введите сообщение</label> -->
+                        <textarea type="text" class="form-control" id="exampleInputTextArea1" rows="3" placeholder="Введите текст сообщения"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-2">Отправить</button>
+                </form>
+            </div>
+            <div class="footer-copirating col-12 p-2">
+                <h6 class="text-center">Copirating&#169; 2020-<?= date('Y') ?>. Design by Yurov</h6>
+            </div>
         </footer>
     </div>
 
