92ad4084e61c55daf609d357b4e0f4713e0fa291
Vladimir-sy <yurov.ssi@gmail.com>
2024-06-25 14:13:03 +0500

    edit files

--- index.php
+++ index.php
@@ -43,4 +43,8 @@ if ($url == '/') {
 
 $page_title = strtoupper($page);
 
-require_once 'templates/main.php';
+if ($url == '/') {
+    require_once 'templates/content.php';
+} elseif (file_exists("templates/$page.php")) {
+    require_once "templates/$page.php";
+}
--- templates/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($url == '/') {
-    require_once 'templates/content.php';
-} elseif (file_exists("templates/$page.php")) {
-    require_once "templates/$page.php";
-}
